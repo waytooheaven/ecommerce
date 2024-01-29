17d671631ed87ad5d1776792e182a6fcd4767ef0
waytooheaven <anilcodingninja@yahoo.com>
2024-01-29 14:18:44 +0300

    code cleanup

--- Ecommerce/Configurations/OrderConfiguration.cs
+++ Ecommerce/Configurations/OrderConfiguration.cs
@@ -10,8 +10,8 @@ public class OrderConfiguration : IEntityTypeConfiguration<Order>
     {
         builder.ToTable(nameof(Order));
         builder.HasKey(x => x.Id);
-        
-        builder.HasOne(x => x.Product).WithMany(x => x.Orders).HasForeignKey(x => x.ProductId); 
-        builder.HasOne(x => x.Customer).WithMany(x => x.Orders).HasForeignKey(x => x.CustomerId); 
+
+        builder.HasOne(x => x.Product).WithMany(x => x.Orders).HasForeignKey(x => x.ProductId);
+        builder.HasOne(x => x.Customer).WithMany(x => x.Orders).HasForeignKey(x => x.CustomerId);
     }
 }
